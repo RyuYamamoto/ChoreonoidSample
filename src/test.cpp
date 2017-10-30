fad5497e9c70082c52444f22b0bfe5ba98870be2
RyuYamamoto <ron_robotics@yahoo.co.jp>
2017-10-30 19:04:18 +0900

    using Link class

--- src/test.cpp
+++ src/test.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <cstring>
 
+#include "Link.cpp"
 #include <cnoid/Body>
 #include <cnoid/BodyLoader>
-#include <cnoid/Link>
-#include <cnoid/JointPath>
 
 int main()
 {
-	cnoid::JointPath JointPath;
-	cnoid::JointPathPtr JointPathPtr;
 	cnoid::BodyPtr robot;
 	cnoid::BodyLoader bodyloader;
-	cnoid::Link Link;
 
 	std::string model_path("/home/haze/github/VRMLModelLoaderSample/model/JAXON_RED/JAXON_JVRCmain.wrl");
 
 	robot = bodyloader.load(model_path.c_str());
 
+	cit::Link ulink[robot->numJoints()];
 	std::string joint_name[robot->numJoints()];
 	for(std::size_t index=0;index<robot->numJoints();index++){
-		joint_name[index] = robot->link(index)->name();
-		std::cout << joint_name[index] << std::endl;
+		ulink[index].joint_name = robot->link(index)->name();
+		ulink[index].p = robot->link(index)->p();
+		ulink[index].R = robot->link(index)->R();
+		ulink[index].a = robot->link(index)->a();
+		ulink[index].b = robot->link(index)->b();
+		ulink[index].w = robot->link(index)->w();
+		ulink[index].v = robot->link(index)->v();
+		ulink[index].m = robot->link(index)->m();
 	}
 	return 0;
 }
