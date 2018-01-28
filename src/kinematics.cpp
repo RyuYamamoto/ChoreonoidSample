c25725d73b5271fdfc240483364e04747712847b
RyuYamamoto <s1226120za@s.chibakoudai.jp>
2018-01-28 23:55:58 +0900

    calculation inverse kinematics

--- src/kinematics.cpp
+++ src/kinematics.cpp
@@ -13,6 +13,11 @@ double deg2rad(double degree)
     return degree * M_PI / 180.f;
 }
 
+double rad2deg(double radian)
+{
+    return radian * 180.f / M_PI;
+}
+
 int main()
 {
     BodyLoader bodyloader;
@@ -22,17 +27,24 @@ int main()
 
     JointPathPtr lleg = getCustomJointPath(robot, robot->rootLink(), robot->link("LLEG_ANKLE_R"));
 
-    lleg->calcForwardKinematics();
-
-    cout << robot->link("LLEG_ANKLE_R")->p() << endl;
-
     robot->joint(14)->q() = deg2rad(-20);
     robot->joint(16)->q() = deg2rad(40);
     robot->joint(17)->q() = deg2rad(-20);
 
     lleg->calcForwardKinematics();
 
-    cout << robot->link("LLEG_ANKLE_R")->p() << endl;
+    for(int i=13;i<19;i++)
+        cout << rad2deg(robot->joint(i)->q()) << endl;
+    cout << "\n";
+
+    Vector3d ref_pos = robot->link("LLEG_ANKLE_R")->p();
+    Matrix3d ref_rot = robot->link("LLEG_ANKLE_R")->R();
+
+    ref_pos[2] += 0.01;
+    if(lleg->calcInverseKinematics(ref_pos, ref_rot)){
+        for(int i=13;i<19;i++)
+            cout << rad2deg(robot->joint(i)->q()) << endl;
+    }
 
     return 0;
 }
