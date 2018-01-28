f820fa606c13dac1f574d9bb5049d8eb67680a9e
RyuYamamoto <s1226120ZA@s.chibakoudai.jp>
2018-01-28 14:50:47 +0900

    fixed model file

--- src/get_link_info.cpp
+++ src/get_link_info.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	BodyLoader bodyloader;
 
-	std::string model_path("model/SR1.wrl");
+	std::string model_path("/usr/share/choreonoid-1.6/model/SR1/SR1.body");
 	BodyPtr robot = bodyloader.load(model_path.c_str());
 
 	cout << "dof: " << robot->numJoints() << endl;
